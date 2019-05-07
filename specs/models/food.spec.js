15e47d8d55e05c184400ce584aa8cc7ba272639c
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-06 18:09:55 -0600

    Adds expectations for food model spec.
    Co-authored-by: Zach Nager <34927114+nagerz@users.noreply.github.com>

--- specs/models/food.spec.js
+++ specs/models/food.spec.js
@@ -1,42 +1,48 @@
 var shell = require('shelljs');
 var request = require("supertest");
 var app = require('../../app');
-var food = require('../../models').Food;
+var Food = require('../../models').Food;
 var pry = require('pryjs');
 
 describe('Food Model test', () => {
   test('It should exist', () => {
-    const cheetos = Food.create({name: 'Cheetos', calories: 30})
-
-    expect(cheetos).toBeInstanceOf(Food)
+    Food.create({name: 'Cheetos', calories: 30})
+    .then(cheetos => {
+      expect(cheetos).toBeInstanceOf(Food)
+    })
   })
 
   test('It has attributes', () => {
-    const cheetos = Food.create({name: 'Cheetos', calories: 30})
-
-    expect(cheetos.name).toBe('Cheetos')
-    expect(cheetos.calories).toBe(30)
+    Food.create({name: 'Cheetos', calories: 30})
+    .then(cheetos => {
+      expect(cheetos.name).toBe('Cheetos')
+      expect(cheetos.calories).toBe(30)
+    })
   })
 
   test('It cannot be created without a name', () => {
-    const cheetos = Food.create({calories: 30})
-
-    expect(cheetos).not(Food)
+    Food.create({calories: 30})
+    .then(cheetos => {
+      expect(cheetos).not(Food)
+    })
   })
 
   test('It cannot be created without a calories', () => {
-    const cheetos = Food.create({name: "Cheetos"})
-
-    expect(cheetos).not(Food)
+    Food.create({name: "Cheetos"})
+    .then(cheetos => {
+      expect(cheetos).toBeInstanceOf(Food)
+    })
   })
 
   test('It cannot be created with a duplicate name and is case insensitive', () => {
-    const cheetos = Food.create({name: 'Cheetos', calories: 30})
+    Food.create({name: 'Cheetos', calories: 30})
+    .then(cheetos => {
+      expect(cheetos).toBeInstanceOf(Food)
+    })
 
-    expect(cheetos).toBeInstanceOf(Food)
-
-    const cheetosAgain = Food.create({name: 'cheetos', calories: 30})
-
-    expect(cheetosAgain).not(Food)
+    Food.create({name: 'cheetos', calories: 30})
+    .then(cheetosAgain => {
+      expect(cheetosAgain).not(Food)
+    })
   })
 })
