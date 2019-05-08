f1b4d1d1330ccb01540b8a69c1203065cd33fa78
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-07 19:23:28 -0600

    Integrates functionality for POST to create a food. Comments out async await version, this is the ideal refactor, but it is returning an unresolved promise for the Food.findOrCreate.

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -1,12 +1,11 @@
 var express = require("express");
 var router = express.Router();
 var Food = require('../../../models').Food;
-var pry = require('pryjs');
 const fetch = require('node-fetch');
+var pry = require('pryjs');
 
 router.get("/", function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
-
   Food.findAll()
   .then(foods => {
     res.status(200).send(JSON.stringify(foods));
@@ -18,7 +17,6 @@ router.get("/", function(req, res, next) {
 
 router.get("/:id", function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
-
   Food.findOne({
     where: {
       id: req.params.id
@@ -36,19 +34,42 @@ router.get("/:id", function(req, res, next) {
   });
 });
 
-router.post("/", async (req, res, next) => {
-  try {
-    eval(pry.it)
-    const food =  await Food.findOrCreate({where: {name: req.body.name, calories: req.body.calories} })
-    if (!food) {
+router.post("/", function(req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  Food.findOrCreate({where: {name: req.body.name, calories: req.body.calories} })
+  .then(food => {
+    if (!food){
       res.status(400).send({ error })
     } else {
-      res.status(200).send(JSON.stringify(food))
+      res.status(200).send(JSON.stringify(parsedFood(food[0])))
     }
-  }
-  catch {
+  })
+  .catch(error => {
     res.status(500).send({ error })
-  }
+  })
 })
+
+function parsedFood(food) {
+    return {"id": food.id,
+    "name": food.name,
+    "calories": food.calories}
+}
+
+
+
+
+// router.post("/", async (req, res, next) => {
+//   try {
+//     const food =  await Food.findOrCreate({where: {name: req.body.name, calories: req.body.calories} })
+//     if (!food) {
+//       res.status(400).send({ error })
+//     } else {
+//       res.status(200).send(JSON.stringify(food))
+//     }
+//   }
+//   catch {
+//     res.status(500).send({ error })
+//   }
+// })
 
 module.exports = router;
