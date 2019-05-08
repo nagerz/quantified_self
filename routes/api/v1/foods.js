63f515358c4591ce1a43d6468e8d5f8cee82bcae
nagerz <znager@gmail.com>
2019-05-08 16:27:18 -0600

    modifies findOrCreate method

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var Food = require('../../../models').Food;
 const fetch = require('node-fetch');
+var pry = require('pryjs');
 
 router.get("/", function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
@@ -42,34 +43,43 @@ router.get("/:id", function(req, res, next) {
 router.post("/", function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
   const calOrNameErrorMessage = "Name/Calories must be passed in to the body via x-www-form-urlencoded in the format of name or calories as the key and item name or calories count as the value without quotes"
-  if (req.body.name === undefined) {
+  //Use validate request here
+  if (!req.body.name || !req.body.calories) {
     res.status(400).send(JSON.stringify(calOrNameErrorMessage))
   } else {
     const name = downCase(req.body.name)
     Food.findOrCreate({
-        where: {name: name},
-        defaults: {calories: req.body.calories}
-      })
-      .then(food => {
-        if (!food) {
-          res.status(400).send({
-            error
-          })
-        } else {
-          res.status(200).send(JSON.stringify(parsedFood(food[0])))
-        }
-      })
-      .catch(error => {
-        if (error.message.includes("WHERE parameter")) {
-          res.status(400).send(JSON.stringify(calOrNameErrorMessage))
-        } else if (typeof(req.body.calories) === "string") {
-          res.status(400).send(JSON.stringify("Please pass the calories datatype as a Number"))
-        } else {
-          res.status(500).send({
-            error
-          })
-        }
-      })
+      where: {name: name},
+      defaults: {calories: req.body.calories}
+    })
+    .spread((food, created) => {
+      if (created) {
+        res.status(200).send(JSON.stringify(food))
+      }
+      res.status(400).send({error:"Food already exists."})
+    })
+
+    // .then(food => {
+    //   if (!food) {
+    //     res.status(400).send({
+    //       error
+    //     })
+    //   } else {
+    //     res.status(200).send(JSON.stringify(parsedFood(food[0])))
+    //   }
+    // })
+
+    .catch(error => {
+      if (error.message.includes("WHERE parameter")) {
+        res.status(400).send(JSON.stringify(calOrNameErrorMessage))
+      } else if (typeof(req.body.calories) === "string") {
+        res.status(400).send(JSON.stringify("Please pass the calories datatype as a Number"))
+      } else {
+        res.status(500).send({
+          error
+        })
+      }
+    })
   }
 })
 
