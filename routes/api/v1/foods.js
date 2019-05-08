c437d90c86f0f5fd5d492edd88640a29cf3bc8d0
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-08 11:49:25 -0600

    Removes comments and utilizes Beautify.

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -7,81 +7,86 @@ var pry = require('pryjs');
 router.get("/", function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
   Food.findAll()
-  .then(foods => {
-    res.status(200).send(JSON.stringify(foods));
-  })
-  .catch(error => {
-    res.status(500).send({error})
-  });
+    .then(foods => {
+      res.status(200).send(JSON.stringify(foods));
+    })
+    .catch(error => {
+      res.status(500).send({
+        error
+      })
+    });
 });
 
 router.get("/:id", function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
   Food.findOne({
-    where: {
-      id: req.params.id
-    }
-  })
-  .then(food => {
-    if(!food){
-      res.status(404).send({ error: "Requested food item could not be found." });
-    }else{
-      res.status(200).send(JSON.stringify(food));
-    }
-  })
-  .catch(error => {
-    res.status(500).send({error})
-  });
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(food => {
+      if (!food) {
+        res.status(404).send({
+          error: "Requested food item could not be found."
+        });
+      } else {
+        res.status(200).send(JSON.stringify(food));
+      }
+    })
+    .catch(error => {
+      res.status(500).send({
+        error
+      })
+    });
 });
 
 router.post("/", function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
   if (req.body.name === undefined) {
-    res.status(400).send({ error })
+    res.status(400).send({
+      error
+    })
   } else {
     const name = upCase(req.body.name)
-    Food.findOrCreate({where: {name: name, calories: req.body.calories} })
-    .then(food => {
-      if (!food){
-        res.status(400).send({ error })
-      } else {
-        res.status(200).send(JSON.stringify(parsedFood(food[0])))
-      }
-    })
-    .catch(error => {
-      console.log(error)
-      if (error.message.includes("WHERE parameter")) {
-        res.status(400).send({ error })
-      } else {
-        res.status(500).send({ error })
-      }
-    })
+    Food.findOrCreate({
+        where: {
+          name: name,
+          calories: req.body.calories
+        }
+      })
+      .then(food => {
+        if (!food) {
+          res.status(400).send({
+            error
+          })
+        } else {
+          res.status(200).send(JSON.stringify(parsedFood(food[0])))
+        }
+      })
+      .catch(error => {
+        if (error.message.includes("WHERE parameter")) {
+          res.status(400).send({
+            error
+          })
+        } else {
+          res.status(500).send({
+            error
+          })
+        }
+      })
   }
 })
 
 function parsedFood(food) {
-    return {"id": food.id,
+  return {
+    "id": food.id,
     "name": food.name,
-    "calories": food.calories}
+    "calories": food.calories
+  }
 }
 
 function upCase(name) {
   return name.charAt(0).toUpperCase() + name.slice(1)
 }
-
-
-// router.post("/", async (req, res, next) => {
-//   try {
-//     const food =  await Food.findOrCreate({where: {name: req.body.name, calories: req.body.calories} })
-//     if (!food) {
-//       res.status(400).send({ error })
-//     } else {
-//       res.status(200).send(JSON.stringify(food))
-//     }
-//   }
-//   catch {
-//     res.status(500).send({ error })
-//   }
-// })
 
 module.exports = router;
