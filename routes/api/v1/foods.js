8eb79b76b715ff4b58f23798a88274a35f9e3240
nagerz <znager@gmail.com>
2019-05-08 14:59:13 -0600

    Adds validateRequest funciton to validate food request formatting. Adds attributes designation to food show and index to return only desired attributes

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -8,7 +8,9 @@ const fetch = require('node-fetch');
 router.get("/", async function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
 
-  Food.findAll()
+  Food.findAll({
+    attributes: ['id', 'name', 'calories']
+  })
   .then(foods => {
     res.status(200).send(JSON.stringify(foods));
   })
@@ -22,7 +24,8 @@ router.get("/:id", async function(req, res, next) {
   return Food.findOne({
     where: {
       id: req.params.id
-    }
+    },
+    attributes: ['id', 'name', 'calories']
   })
   .then(food => {
     if(!food){
@@ -38,23 +41,52 @@ router.get("/:id", async function(req, res, next) {
 
 router.patch("/:id", function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
-  if(req.body.food && req.body.food.name && req.body.food.calories){
-    Food.update(req)
-    .then(food => {
-      if(!food){
-        res.status(404).send({ error: "Requested food item could not be found." });
-      }else if(food.error){
-        res.status(404).send({ error: food.error.errors[0].message});
-      }else{
-        res.status(200).send(JSON.stringify(food));
-      }
-    })
-    .catch(error => {
-      res.status(500).send({error})
-    });
-  }else{
-    res.status(400).send({ error: "Request formatted incorrectly and/or missing information." });
-  }
+
+  validateRequest(req)
+  .then(req => {
+    if(req.body.food && req.body.food.name && req.body.food.calories){
+      Food.update(req)
+      .then(food => {
+        if(!food){
+          res.status(404).send({ error: "Requested food item could not be found." });
+        }else if(food.error){
+          res.status(404).send({ error: food.error.errors[0].message});
+        }else{
+          res.status(200).send(JSON.stringify(food));
+        }
+      })
+      .catch(error => {
+        res.status(500).send({error})
+      });
+    }else{
+      res.status(400).send({ error: "Request formatted incorrectly and/or missing information." });
+    }
+  })
+  .catch(error => {
+    res.status(404).send({ error: error });
+  })
+
 });
+
+function validateRequest(req) {
+  return new Promise((resolve, reject) => {
+    if (req.body.food){
+      if (req.body.food.name && req.body.food.calories){
+        if (Number.isInteger(req.body.food.calories) === true){
+          resolve(req)
+        }else{
+          error = "Invalid calories. Must be integer."
+          reject(error)
+        }
+      }else{
+        error = "Missing information."
+        reject(error)
+      }
+    }else{
+      error = "Request missing food designation."
+      reject(error)
+    }
+  })
+}
 
 module.exports = router;
