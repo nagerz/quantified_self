1f9c1f403c6e086c3712a21161c0e9c21ca51c56
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-09 17:18:14 -0600

    Progresses on implementation of meal DELETE spec.

--- specs/endpoints/meal_delete.spec.js
+++ specs/endpoints/meal_delete.spec.js
@@ -1,6 +1,8 @@
 var shell = require('shelljs');
 var request = require("supertest");
 var app = require('../../app');
+var Meal = require('../../../models').Meal;
+var MealFood = require('../../../models').MealFood;
 
 describe('Meal delete API', () => {
   beforeAll(() => {
@@ -17,25 +19,24 @@ describe('Meal delete API', () => {
 
   describe('Test DELETE /api/v1/meals/:meal_id/foods/:id', () => {
     test('it should return a 204 status', () => {
-      Meals.findAll(include: [{model: Food}])
-      .then(meals => {
-
-      })
+      // Meals.findAll(include: [{model: Food}])
+      // .then(meals => {
+      //
+      // })
 
       return request(app).delete("/api/v1/meals/1/foods/1").then(response => {
         expect(response.status).toBe(200)
-        expect(response.error).toBe("{error: 'The meal has been successfully deleted'}")
-        Meals.findAll(include: [{model: Food}]).count
-        .then(meals => {
-
-        })
+        expect(response.error).toBe("{error: 'The food has been successfully deleted from the meal.'}")
+        // Meals.findAll(include: [{model: Food}]).count
+        // .then(meals => {
+        //
+        // })
       })
     })
 
     test('it should return a 404 status', () => {
-      return request(app).get("/api/v1/meals/1").then(response => {
-        expect(response.status).toBe(200)
-        expect(response.error).toBe('')
+      return request(app).delete("/api/v1/meals/1/foods/1").then(response => {
+        expect(response.status).toBe(404)
       })
     })
   })
