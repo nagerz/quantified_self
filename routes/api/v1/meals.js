e09087081ce86f810320d32fe5d8477e18ee685a
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-09 14:50:40 -0600

    Begins implementation for route to DELETE a MealFood.

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -3,26 +3,87 @@ var router = express.Router();
 var Food = require('../../../models').Food;
 var Meal = require('../../../models').Meal;
 const fetch = require('node-fetch');
+var pry = require('pryjs');
+
 
 router.get("/:id/foods", async function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
   Meal.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: ['id', 'name'],
-    include: [{model:Food, attributes: ['id', 'name', 'calories'], through: { attributes: []}}]
-  })
-  .then(meal => {
-    if(!meal){
-      res.status(404).send({ error: "Requested meal could not be found." });
-    }else{
-      res.status(200).send(JSON.stringify(meal));
-    }
-  })
-  .catch(error => {
-    res.status(404).send({error})
-  });
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'name'],
+      include: [{
+        model: Food,
+        attributes: ['id', 'name', 'calories'],
+        through: {
+          attributes: []
+        }
+      }]
+    })
+    .then(meal => {
+      if (!meal) {
+        res.status(404).send({
+          error: "Requested meal could not be found."
+        });
+      } else {
+        res.status(200).send(JSON.stringify(meal));
+      }
+    })
+    .catch(error => {
+      res.status(404).send({
+        error
+      })
+    });
 });
+
+router.delete('/:id/foods/:id', function(req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  const mealIdParam = req.params.id
+  Meal.findOne({
+      where: {
+        id: mealIdParam
+      },
+      include: [{
+        model: Food
+      }]
+    })
+    .then(meal => {
+      const foodIdParam = req.url.slice(9)
+      meal.dataValues.Food.forEach(function(food) {
+        eval(pry.it)
+        if (food.dataValues.id === foodIdParam) {
+          MealFood.destroy({
+            where: {
+              MealId: mealIdParam,
+              FoodId: foodIdParam
+            }
+          })
+          // resolve promise
+          // send response
+        } else {
+          // return eror about food record not found
+        }
+        // Catch statments
+      })
+
+
+
+
+
+      if (!meal) {
+        res.status(404).send({
+          error: "Requested record could not be found."
+        });
+      } else {
+        res.status(200).send(JSON.stringify(meal));
+      }
+    })
+    .catch(error => {
+      res.status(404).send({
+        error
+      })
+    });
+})
 
 module.exports = router;
