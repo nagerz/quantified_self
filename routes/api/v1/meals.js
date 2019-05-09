e86f6d10271c86dbd433272e4e02cff54ebc4c06
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-05-09 12:02:25 -0600

    Implements functionality for meal index. All tests passing. Expected 200 behavior observed in Postman.

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -7,22 +7,62 @@ const fetch = require('node-fetch');
 router.get("/:id/foods", async function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
   Meal.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: ['id', 'name'],
-    include: [{model:Food, attributes: ['id', 'name', 'calories'], through: { attributes: []}}]
-  })
-  .then(meal => {
-    if(!meal){
-      res.status(404).send({ error: "Requested meal could not be found." });
-    }else{
-      res.status(200).send(JSON.stringify(meal));
-    }
-  })
-  .catch(error => {
-    res.status(404).send({error})
-  });
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'name'],
+      include: [{
+        model: Food,
+        attributes: ['id', 'name', 'calories'],
+        through: {
+          attributes: []
+        }
+      }]
+    })
+    .then(meal => {
+      if (!meal) {
+        res.status(404).send({
+          error: "Requested meal could not be found."
+        });
+      } else {
+        res.status(200).send(JSON.stringify(meal));
+      }
+    })
+    .catch(error => {
+      // Should this be a 500 internal server error?
+      res.status(404).send({
+        error
+      })
+    });
 });
+
+router.get("/", async function(req, res, next) {
+  res.setHeader("content-Type", "application/json");
+  Meal.findAll({
+    attributes: ['id', 'name'],
+    include: [{
+      model: Food,
+      attributes: ['id', 'name', 'calories'],
+      through: {
+        attributes: []
+      }
+    }]
+  })
+    .then(meals => {
+      if (!meals) {
+        res.status(404).send({
+          error: 'There are no meals in the database.'
+        })
+      } else {
+        res.status(200).send(JSON.stringify(meals))
+      }
+    })
+    .catch(error => {
+      res.status(500).send({
+        error
+      })
+    })
+})
+
 
 module.exports = router;
