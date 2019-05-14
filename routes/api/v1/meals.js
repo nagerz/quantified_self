c5f92a7319e95e3f6573e19c8f1f662bfbab008c
nagerz <znager@gmail.com>
2019-05-14 10:54:10 -0600

    Fixes relationship error in recipe model. Adds endpoint for adding recipe to a meal

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -2,9 +2,11 @@ var express = require("express");
 var router = express.Router();
 var Food = require('../../../models').Food;
 var Meal = require('../../../models').Meal;
+var Recipe = require('../../../models').Recipe;
 var MealFood = require('../../../models').MealFood;
+var MealRecipe = require('../../../models').MealRecipe;
 const fetch = require('node-fetch');
-//var pry = require('pryjs');
+var pry = require('pryjs');
 
 router.get("/:id/foods", async function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
@@ -118,6 +120,66 @@ router.post("/:meal_id/foods/:food_id", async function(req, res, next) {
   .catch(error => {
     res.status(400).send({ error: "Invalid request." });
   })
+});
+
+router.post("/:meal_id/recipe", async function(req, res, next) {
+  res.setHeader("content-Type", "application/json");
+  validateRecipeRequest(req)
+  .then(req => {
+    Meal.findOne({
+      where: {
+        id: req.params.meal_id
+      },
+      attributes: ['id', 'name']
+    })
+    .then(meal => {
+      if (!meal) {
+        res.status(404).send({
+          error: 'No meal with that ID.'
+        })
+      } else {
+        Recipe.findOrCreate({
+          where: {name: req.body.recipe.name},
+          defaults: {
+            calories: req.body.recipe.calories,
+            url: req.body.recipe.url
+          },
+          attributes: ['id', 'name']
+        })
+        .then(recipe => {
+          if(!recipe) {
+            res.status(404).send({error: 'Invalid recipe information.'})
+          }else{
+            MealRecipe.findOrCreate({
+              where: {
+                MealId: meal.id,
+                RecipeId: recipe[0].id
+              }
+            })
+            .spread((mealrecipe, created) => {
+              if (created) {
+                res.status(201).send(JSON.stringify({"message": `Successfully added ${recipe[0].name} to ${meal.name}`}))
+              }else{
+                res.status(400).send({error:"That recipe already exists for that meal."})
+              }
+            })
+            .catch(error => {
+              res.status(400).send({ error: error });
+            })
+          }
+        })
+        .catch(error => {
+          res.status(400).send({ error: "Invalid request." });
+        })
+      }
+    })
+    .catch(error => {
+      res.status(400).send({ error: "Invalid meal request." });
+    })
+  })
+  .catch(error => {
+    res.status(404).send(JSON.stringify({ error: error }))
+  })
 })
 
 router.delete('/:meal_id/foods/:food_id', function(req, res, next) {
@@ -138,6 +200,28 @@ router.delete('/:meal_id/foods/:food_id', function(req, res, next) {
   .catch(error => {
     res.status(404).send(JSON.stringify({ error: "Invalid request." }))
   })
-})
+
+});
+
+function validateRecipeRequest(req) {
+  return new Promise((resolve, reject) => {
+    if (req.body.api_key){
+      if (req.body.recipe && req.body.recipe.name && req.body.recipe.calories && req.body.recipe.url){
+        if (Number.isInteger(req.body.recipe.calories) === true){
+          resolve(req)
+        }else{
+          error = "Invalid calories. Must be integer."
+          reject(error)
+        }
+      }else{
+        error = "Missing/incorrectly formatted recipe information."
+        reject(error)
+      }
+    }else{
+      error = "Missing api key."
+      reject(error)
+    }
+  })
+};
 
 module.exports = router;
